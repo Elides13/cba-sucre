be39e67fa2abbc19ee464cdd7b3abd0e9169c4f8
Elides13 <cerezo.1617@gmail.com>
2025-04-10 15:05:07 -0400

    estructura

---
+++ frontend_cba/src/views/SobreNosotros.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+      <h1>Sobre Nosotros</h1>
+      <p>El CBA Sucre es una institución dedicada a la enseñanza del inglés...</p>
+    </div>
+  </template>
+  
+  <style scoped>
+  .container {
+    padding: 20px;
+    max-width: 800px;
+    margin: auto;
+  }
+  h1 {
+    color: #007bff;
+  }
+  </style>
+  
